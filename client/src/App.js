08365905eccc9f3383017fbc82d1d7727c5b73ec
Lee Rogers <edwinleerogers@gmail.com>
2020-03-28 18:00:31 -0700

    refactor app.js to make functional component with hooks, using axios/async/await

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  // Initialize state
-  state = { passwords: [] }
+function App() {
+  const [passwords, setPasswords] = useState([]);
 
-  // Fetch passwords after first mount
-  componentDidMount() {
-    this.getPasswords();
-  }
+  useEffect(() => {
+    getPasswords();
+  }, [])
 
-  getPasswords = () => {
+  const getPasswords = async () => {
     // Get the passwords and store them in state
-    fetch('/api/passwords')
-      .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
+    try {
+      const {data} = await axios('/api/passwords');
+      setPasswords(data);
+    } catch (err) {
+      console.error(err);
+    }
   }
+    
 
-  render() {
-    const { passwords } = this.state;
-
-    return (
-      <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {/* Render the passwords if we have them */}
+      {passwords.length ? (
+        <div>
+          <h1>5 Passwords.</h1>
+          <ul className="passwords">
+            {/*
+              Generally it's bad to use "index" as a key.
+              It's ok for this example because there will always
+              be the same number of passwords, and they never
+              change positions in the array.
+            */}
+            {passwords.map((password, index) =>
+              <li key={index}>
+                {password}
+              </li>
+            )}
+          </ul>
+          <button
+            className="more"
+            onClick={getPasswords}>
+            Get More
+          </button>
+        </div>
+      ) : (
+        // Render a helpful message otherwise
+        <div>
+          <h1>No passwords :(</h1>
+          <button
+            className="more"
+            onClick={getPasswords}>
+            Try Again?
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
